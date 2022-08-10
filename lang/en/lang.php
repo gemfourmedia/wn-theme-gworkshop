017cb586c6a9cbf6dcdc3eda5ea71d11f4c819ae
Gem Four Media <gemfourmedia@gmail.com>
2022-08-10 22:22:02 +0700

    Init

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,7 +3,19 @@
 return [
     'options' => [
         'googleanalytics_id' => 'Google Analytics ID',
+        'fb_app_id' => 'Facebook app ID',
         'color_primary' => 'Primary Color',
         'color_secondary' => 'secondary Color',
     ],
+    'posts' => [
+        'readmore' => 'Read more',
+    ],
+    'post' => [
+        'toc' => 'Table of contents',
+        'share' => 'Share',
+        'print' => 'Print',
+        'email' => 'Email',
+        'next' => 'Next',
+        'previous' => 'Previous',
+    ]
 ];
